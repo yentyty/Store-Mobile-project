cd50756aa3941fb11a0b887d35d9a3c41ffd0987
yentyty <nguyenthiyen.tk20@gmail.com>
2019-03-19 12:35:22 +0700

    BE edit user

--- app/Http/Controllers/V1/Web/backend/UserController.php
+++ app/Http/Controllers/V1/Web/backend/UserController.php
@@ -123,6 +123,8 @@ class UserController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $this->repoUser->delete($id);
+
+        return redirect()->route('user.index')->with('msg', 'Delete successful');
     }
 }
